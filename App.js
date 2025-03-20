9ace084b210406eb782aa290001b5e80138ce253
summitaiAU <kirt.quar@summitai.com.au>
2025-03-21 10:37:03 +1100

    Add main App.js component with SafeAreaProvider and StatusBar

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import CalculatorScreen from './src/screens/CalculatorScreen';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="light" />
+      <CalculatorScreen />
+    </SafeAreaProvider>
+  
